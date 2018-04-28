82632d438f7460a1e5233d5339515a17e8768f80
Anomvremio <heartvestor@gmail.com>
2018-04-28 19:30:11 +0100

    modified menu style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Parallax } from 'react-materialize';
+import { Parallax, Dropdown, Button } from 'react-materialize';
 
 const Header = () => (
-    <div className="container-fluid ">
-        <nav className="yellow accent-4">
-            <div className="nav-wrapper container">
-                <a href="" className="brand-logo black-text">NavMe</a>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><NavLink to="/" activeClassName="is-active" className="black-text" exact={true}>Home</NavLink></li>
-                    <li><NavLink to="/portfolio" activeClassName="is-active" className="black-text">Portfolio</NavLink></li>
-                    <li><NavLink to="/contact" activeClassName="is-active" className="black-text">Contact</NavLink></li>                  
-                </ul>
-            </div>
-        </nav>
-        <Parallax imageSrc="assets/images/bg6.jpg" alt="banner" />
-      
+    <div className="container-fluid center">
+        <Dropdown trigger={
+            <Button className="amber btn-large waves-effect waves-light topmargin bottommargin"><i className="material-icons black-text">menu</i></Button>
+        }>
+            <li><NavLink to="/" activeClassName="is-active" className="black-text" exact={true}>Home</NavLink></li>
+            <li><NavLink to="/portfolio" activeClassName="is-active" className="black-text">Portfolio</NavLink></li>
+            <li><NavLink to="/contact" activeClassName="is-active" className="black-text">Contact</NavLink></li> 
+        </Dropdown>
+        <Parallax imageSrc="assets/images/bg6.jpg" alt="banner" /> 
     </div>
 );
 
