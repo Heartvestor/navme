2f59d9eab126bcf7832beda2a3a10434d0395653
Anomvremio <heartvestor@gmail.com>
2018-05-20 22:36:15 +0100

    update to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,7 +10,7 @@ const Portfolio = () => (
                     <p className="center"><img src="assets/images/me.jpg" width="64px" height="64px" alt="" className="circle responsive-img" /></p>
                     <blockquote className="left-align light">Since late 2012 I've carried out a deal of personal tech projects I've shutdown, among which ecommerce, academic - professional, social networking...
                     <br />While in the runs for bootstrapping a new startup and get it propulsed, or a cool job opportunity, I offer professional services as a consultant.<br />
-                    The following are some client websites actually online, with some yet to be online.
+                    The following are some client websites actually online, with some yet to be online. A mobile app too, not deployed to the store yet (community app). Also listed are some personal sites.
                     </blockquote>
                     </div>
                 </div>
@@ -21,12 +21,17 @@ const Portfolio = () => (
             <div className='amber white-text center'>
                 <h2>PalsBook</h2>
                 <p className="black-text">About Pals (https://palsbook-7aff7.firebaseapp.com/)</p>
-                <p><img className="responsive-img" src="assets/images/mitziks.png" alt="miziks" /></p>
+                <p><img className="responsive-img" src="assets/images/palsbook.png" alt="palsbook" /></p>
             </div>
             <div className='amber white-text center'>
                 <h2>WalterWilson.Space</h2>
                 <p className="black-text">Of course, this too! ;-)</p>
                 <p><img className="responsive-img" src="assets/images/navme.png" alt="navme" /></p>
+            </div>
+            <div className='amber white-text center'>
+                <h2>BDF Connect</h2>
+                <p className="black-text">An app for the Benin Dns Forum community.</p>
+                <p><img className="responsive-img" src="assets/images/bdfconnect.png" alt="bdfconnect" /></p>
             </div>
             <div className='amber white-text center'>
                 <h2>MitZiks.Com</h2>
